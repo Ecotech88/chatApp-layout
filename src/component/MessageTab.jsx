727640d51d8dc782cdd4000349dee86eb1f1999f
Adelani <adeoyejoel121@gmail.com>
2025-03-17 13:34:56 +0100

    Update MessageTab.jsx
    nbb

--- src/component/MessageTab.jsx
+++ src/component/MessageTab.jsx
@@ -3,7 +3,7 @@ import Chats from './Chats.jsx'
 
 const MessageTab = () => {
   return (
-    <div className=' h-screen  gap-1 '>
+    <div className='lg:flex item-center  h-screen  gap-1 '>
         <SideBar/>
         <Chats/>
     </div>
