1c344d7098be1a461734bf10912e64022ba74a4c
Adelani <adeoyejoel121@gmail.com>
2025-03-17 12:33:07 +0100

    Update SideBar.jsx
    change

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -3,7 +3,7 @@ import { Users } from '../constant/Data';
 
 const SideBar = () => {
   return (
-    <div className='flex flex-col justify-between lg:max-w-[500px] border-yellow-50 max-h-screen border-r-2 lg:shadow-2xl md:w-full'>
+    <div className='flex flex-col justify-between lg:max-w-[300px] border-yellow-50 max-h-screen border-r-2 lg:shadow-xl md:w-full'>
       <div className="md:p-4 flex ml-1 justify-between items-center">
         <img className='shadow-lg shadow-gray-600 w-8 bg-white border-none rounded-full p-1 cursor-pointer' src={backIcon} alt="" />
       </div>
